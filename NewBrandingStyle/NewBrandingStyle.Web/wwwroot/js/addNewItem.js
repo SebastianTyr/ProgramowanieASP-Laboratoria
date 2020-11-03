bf91c16ba68ce3601bc13704e844790c6c06bb83
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2020-11-04 00:00:05 +0100

    AJAX added and need to be fixed

---
+++ NewBrandingStyle/NewBrandingStyle.Web/wwwroot/js/addNewItem.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    const alertElement = document.getElementById("success-alert");
+    const formElement = document.forms[0];
+
+    const addNewItem = async () => {
+        //1 reading form data
+        const requestData = formElement.nodeValue;
+
+        //2 calling application server using fetch API method
+        const response = await fetch(requestData);
+
+        const responseJSON = await response.json();
+
+        if (responseJSON.success) {
+            //3 unhide element
+            alertElement.style.display = "flex";
+
+        }
+    };
+
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+}) ();
